fdaae4a70bd36a5ef9b0eb99219dee6de965230f
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2024-06-25 16:59:41 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -35,4 +35,56 @@ function generatePassword() {
 
 function copyPassword(){
     alert("senha copiada com sucesso")
+
+    navigator.clipboard.writeText(novaSenha);
 }
+
+const passwordInput = document.getElementById("password-input");
+const submitButton = document.getElementById("submit-button");
+const uppercaseRequirement = document.getElementById("uppercase-requirement");
+const lowercaseRequirement = document.getElementById("lowercase-requirement");
+const numbersRequirement = document.getElementById("numbers-requirement");
+const symbolsRequiremnet = document.getElementById("symbols-requirement");
+const lengthRequirement = document.getElementById("length-requirement");
+
+
+passwordInput.addEventListener('input', checkPassword);
+
+function checkPassword(){
+    const passwordValid = passwordInput.value;
+
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const numbersRegex = /\d/;
+    const symbolsRegex = /\W/;
+
+
+    const uppercaseFulfilled = uppercaseRegex.test(passwordValid)
+    const lowercaseFulfilled = lowercaseRegex.test(passwordValid)
+    const numberseFulfilled = numbersRegex.test(passwordValid)
+    const symbolsFulfilled = symbolsRegex.test(passwordValid)
+    const lengthFulfilled = passwordValid.length >= 8;
+
+    uppercaseRequirement.classList.toggle('fulfilled', uppercaseFulfilled)
+    lowercaseRequirement.classList.toggle('fulfilled', lowercaseFulfilled)
+    numbersRequirement.classList.toggle('fulfilled', numberseFulfilled)
+    symbolsRequiremnet.classList.toggle('fulfilled', symbolsFulfilled)
+    lengthRequirement.classList.toggle('fulfilled', lengthFulfilled)
+
+    const isButtonDisabled = 
+    !uppercaseFulfilled||
+    !lowercaseFulfilled||
+    !numberseFulfilled||
+    !symbolsFulfilled||
+    !lengthFulfilled
+    ;
+
+    submitButton.disabled = isButtonDisabled;
+
+
+}
+
+submitButton.addEventListener('click', function(){
+    console.log('botão clicado!');
+})
+
